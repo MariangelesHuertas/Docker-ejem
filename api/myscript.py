457bcda8dc9e3a1d95dec4bf043fe35acb116b74
MariangelesHuertas <mariangeles.huertas@tecsup.edu.pe>
2023-12-19 00:07:28 -0500

    actualizacion

--- api/myscript.py
+++ api/myscript.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def get_ratings(cantidad=5):  # Añade un parámetro para la cantidad de datos a mostrar
     # Cargar el archivo CSV
-    ratings = pd.read_csv('MovieLens-100K_Recommender-System/data/ratings.csv')
+    ratings = pd.read_csv('ratings.csv')
     return ratings.head(cantidad).to_dict(orient='records')
